2a1f59b7e6e1dc98124a7858deab3425684d7cde
Fabricio Cosati <fabricio18cosati@gmail.com>
2022-01-13 21:36:23 -0300

    upadte & delete

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -8,6 +8,7 @@ use App\Models\User;
 
 class EventController extends Controller
 {
+
     public function index() {
         
         $search = request("search");
@@ -67,5 +68,49 @@ class EventController extends Controller
         $eventOwner = User::where('id', $event->user_id)->first()->toArray();
 
         return view("events.show", ["event" => $event, "eventOwner" => $eventOwner]);
+    }
+
+    public function dashboard() {
+        $user = auth()->user();
+
+        $events = $user->events;
+
+        return view("events.dashboard", ["events" => $events]);
+    }
+
+    public function destroy($id) {
+        Event::findOrFail($id)->delete();
+
+        return redirect("/dashboard")
+        ->with("msg", "Evento excluido com sucesso");
+    }
+
+    public function edit($id) {
+        $event = Event::findOrFail($id);
+
+        return view("events.edit", ["event" => $event]);
+    }
+
+    public function update(Request $req) {
+
+        $data = $req->all();
+
+        if($req->hasFile('image') && $req->file('image')->isValid()) {
+
+            $reqImage = $req->image;
+
+            $extension = $reqImage->extension();
+
+            $imageName = md5($reqImage->getClientOriginalName() . strtotime("now")) . "." . $extension;
+
+            $reqImage->move(public_path("img/events"), $imageName);
+
+            $data['image'] = $imageName;
+        }
+
+        Event::findOrFail($req->id)->update($data);
+
+        return redirect("/dashboard")
+        ->with("msg", "Evento editado com sucesso!");
     }
 }
